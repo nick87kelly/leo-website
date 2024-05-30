4bafd5d4436ff8fa250769365bb70e539c0ac56f
Nick Kelly <nbkelly2@asu.edu>
2024-05-29 21:36:29 -0700

    added loader to icon

--- src/components/IconSection.js
+++ src/components/IconSection.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 const logo = require("../assets/misc/cameraAnim.webm");
 const sparkle = require("../assets/misc/sparkle.png");
+const defaultLogo = require("../assets/profilePics/default.png");
 
 const IconSection = () => {
   const sparkleRef = useRef();
+  const [loader, setLoader] = useState(true);
   useEffect(() => {
     const interval = setInterval(() => {
       sparkleRef.current.style.top = Math.random() * 80 + "%";
@@ -15,10 +17,24 @@ const IconSection = () => {
 
   return (
     <div id="icon-section">
-      <video id="icon" playsInline muted loop autoPlay>
+      <video
+        id="icon"
+        playsInline
+        muted
+        loop
+        autoPlay
+        onCanPlayThrough={() => {
+          setLoader(false);
+        }}
+      >
         <source src={logo}></source>
       </video>
       <img ref={sparkleRef} src={sparkle} id="icon-sparkle" />
+      <img
+        src={defaultLogo}
+        style={{ display: loader ? "block" : "none" }}
+        id="default-loader"
+      />
     </div>
   );
 };
