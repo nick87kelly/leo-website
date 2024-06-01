303152b9cd110bacbe5e7a525f90e4d6e7b47faa
Nick Kelly <nbkelly2@asu.edu>
2024-06-01 01:07:54 -0700

    more contact progress

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,32 +5,37 @@ function Contact() {
   const dnaLogo = require("../assets/misc/dna.webm");
   const defaultLogo = require("../assets/profilePics/default.png");
   const batteryLogo = require("../assets/misc/battery.png");
+  const leftFp = require("../assets/misc/leftFingerprint.png");
+  const rightFp = require("../assets/misc/rightFingerprint.png");
+  const userIdLength = 10;
   const [userId, setUserId] = useState(
     "USR-" +
       [..."abcdefghijklmnopqrsuvwxyzABCDEFGHIJKLMNOPQRSUVWXYZ0123456789"]
         .map((e, i, a) => a[Math.floor(Math.random() * a.length)])
         .join("")
-        .slice(0, 15)
+        .slice(0, userIdLength)
   );
   const [loader, setLoader] = useState(true);
-  //   useEffect(() => {
-  //     const interval = setInterval(() => {
-  //       setUserId(
-  //         "USR-" +
-  //           [..."abcdefghijklmnopqrsuvwxyzABCDEFGHIJKLMNOPQRSUVWXYZ0123456789"]
-  //             .map((e, i, a) => a[Math.floor(Math.random() * a.length)])
-  //             .join("")
-  //             .slice(0, 15)
-  //       );
-  //     }, 200);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      var newChar = [
+        ..."abcdefghijklmnopqrsuvwxyzABCDEFGHIJKLMNOPQRSUVWXYZ0123456789",
+      ]
+        .map((e, i, a) => a[Math.floor(Math.random() * a.length)])
+        .join("")
+        .slice(0, 1);
+      var newIndex = Math.floor(Math.random() * userIdLength + 4);
 
-  //     return () => clearInterval(interval);
-  //   }, []);
+      setUserId(
+        (prev) =>
+          prev.substring(0, newIndex) + newChar + prev.substring(newIndex + 1)
+      );
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <>
-      {/* <video playsInline autoPlay muted loop id="myVideo">
-        <source src={require("../assets/misc/bg7.mp4")} type="video/mp4" />
-      </video> */}
       <div id="contact-main-container">
         <div id="contact-header">
           <div id="contact-name">LEONARDO GIRON</div>
@@ -41,8 +46,14 @@ function Contact() {
             alt="uh oh"
           />
         </div>
-        <div id="left-fingerprint"></div>
-        <div id="right-fingerprint"></div>
+        <div id="left-fingerprint">
+          <div className="fingerprint-info">L</div>
+          <img src={leftFp} className="fingerprint-img" alt="uh oh" />
+        </div>
+        <div id="right-fingerprint">
+          <div className="fingerprint-info">R</div>
+          <img src={rightFp} className="fingerprint-img" alt="uh oh" />
+        </div>
         <div id="face-scan">
           <video
             id="contact-icon"
@@ -64,7 +75,15 @@ function Contact() {
           />
           <div id="face-scan-bar"></div>
         </div>
-        <div id="contact-random1"></div>
+        <div id="contact-random1">
+          <div id="gender-title">sex:</div>
+          <div className="gender" id="female-gender">
+            F
+          </div>
+          <div className="gender" id="male-gender">
+            M
+          </div>
+        </div>
         <div id="contact-random2">
           <div id="contact-id-title">user-id:</div>
           <div id="contact-id-container">
